547d40700b0171621e25d626b18ba7cf629b461e
Anand kumar <anandkumarsaidi2005@gmail.com>
2025-09-19 22:56:04 +0530

    circular ll done

--- LinkedList/circulartDllAssign.cpp
+++ LinkedList/circulartDllAssign.cpp
@@ -63,6 +63,19 @@ class circularDll{
         }
     }
 
+    //Given a sorted circular doubly linked list of distinct nodes(no two nodes have the
+    // same data) and a value x. Count triplets in the list that sum up to a given value x.
+
+    // void countTriplets(int x){
+
+    // }
+
+    //Given a circular singly linked list containing N nodes, the task is to delete all the odd
+    // nodes from the list
+    void deletOddNodes(){
+        node* temp = head;
+    }
+
     void printCll(){
         node* temp = head;
 
@@ -75,6 +88,32 @@ class circularDll{
 
 };
 
+void isIdentical(circularDll l1, circularDll l2){
+    node* temp1 = l1.head;
+    node* temp2 = l2.head;
+
+    if(temp1 == NULL ){
+        cout<<"list is empty !"<<endl;
+        return ;
+    }
+    while(temp1->next != l1.head && temp2->next != l2.head){
+        if(!(temp1->val == temp2->val)){
+            cout<<"list is not identical !"<<endl;
+            return;
+        }
+
+        temp1 = temp1->next;
+        temp2 = temp2->next;
+    }
+
+    if(!(temp1->val == temp2->val)){
+        cout<<"list is not identical !"<<endl;
+        return;
+    }
+    cout<<"list is identical "<<endl;
+}
+
+
 int main(){
     circularDll cll;
     cll.insert(10);
@@ -83,9 +122,20 @@ int main(){
     cll.insert(13);
     cll.insert(14);
     cll.insert(15);
+    // cll.printCll();
+    // cll.deleteNode(1);
     cll.printCll();
-    cll.deleteNode(1);
-    cll.printCll();
+
+    circularDll cll2;
+    cll2.insert(10);
+    cll2.insert(11);
+    cll2.insert(12);
+    cll2.insert(13);
+    cll2.insert(14);
+    cll2.insert(15);
+    cll2.printCll();
+
+    isIdentical(cll, cll2);
 
     return 0;
 }
