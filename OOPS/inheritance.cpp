afdb06461ca21897babfe03d438d15c690034d50
Anand kumar <anandkumarsaidi2005@gmail.com>
2025-10-19 21:33:05 +0530

    inheritance

---
+++ OOPS/inheritance.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+class Animal{
+    public:
+    void sound(){
+        cout<<"animals have the sound "<<endl;
+    }
+};
+
+class Dog : private Animal{
+    public: 
+    void dog(){
+        sound();
+        cout<<"I'm in the class dog"<<endl;
+    }
+};
+
+int main(){
+    Dog d;
+    d.dog();
+    // d.sound();
+    return 0;
+}
