56316316a9220a371724a173cff17b11c1368f09
Anand kumar <anandkumarsaidi2005@gmail.com>
2026-04-19 20:06:32 +0530

    started dp

---
+++ dp/tempCodeRunnerFile.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int fib(int n, vector<int> &dp){
+    if(n<=1) return dp[n]=n;
+
+    if(dp[n] != -1) return dp[n];
+
+    dp[n] = fib(n-1, dp) + fib(n-2, dp);
+
+    return dp[n];
+}
+int main(){
+    int n;
+    cin>>n;
+    vector<int> dp(n+1, -1);
+    fib(n, dp);
+
+    for(auto x: dp){
+        cout<<x<<" ";
+    }
+    
+    return 0;
+}
